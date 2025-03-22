5dcdb67ab4681d3c2645023246e84005068067ad
MazwiMagubane <mazwimagubane@gmail.com>
2025-03-22 06:24:18 +0200

    (1) Added Bootstrap "select box" (2) Added model to track users and associated selected items (3) Added a table (4) Added a chart (5) Added delete functionality

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Food(models.Model):
-    
+
     def __str__(self):
         return self.name
 
@@ -11,3 +12,8 @@ class Food(models.Model):
     protein = models.FloatField()
     fats = models.FloatField()
     calories = models.IntegerField()
+
+class Consume(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    food_consumed = models.ForeignKey(Food, on_delete=models.CASCADE)
+    
